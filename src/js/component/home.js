293a2c49510aac44bd7ed7e3eeb2c34e2ecb0f86
ajiva84 <jivazam@gmail.com>
2020-12-13 16:54:38 +0000

    second commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -24,17 +24,141 @@ export function Home() {
 			category: "game",
 			name: "Mario Overworld",
 			url: "files/mario/songs/overworld.mp3"
+		},
+		{
+			id: 4,
+			category: "game",
+			name: "Mario Stage 1",
+			url: "files/mario/songs/stage1.mp3"
+		},
+		{
+			id: 5,
+			category: "game",
+			name: "Mario Stage 2",
+			url: "files/mario/songs/stage2.mp3"
+		},
+		{
+			id: 6,
+			category: "game",
+			name: "Mario Star",
+			url: "files/mario/songs/starman.mp3"
+		},
+		{
+			id: 7,
+			category: "game",
+			name: "Mario Underworld",
+			url: "files/mario/songs/underworld.mp3"
+		},
+		{
+			id: 8,
+			category: "game",
+			name: "Mario Underwater",
+			url: "files/mario/songs/underwater.mp3"
+		},
+		{
+			id: 9,
+			category: "game",
+			name: "Zelda Castle",
+			url: "files/videogame/songs/zelda_castle.mp3"
+		},
+		{
+			id: 10,
+			category: "game",
+			name: "Zelda Outworld",
+			url: "files/videogame/songs/zelda_outworld.mp3"
+		},
+		{
+			id: 11,
+			category: "game",
+			name: "Zelda Titles",
+			url: "files/videogame/songs/zelda_title.mp3"
+		},
+		{
+			id: 11,
+			category: "game",
+			name: "Sonic Brain Zone",
+			url: "files/videogame/songs/sonic_brain-zone.mp3"
+		},
+		{
+			id: 11,
+			category: "game",
+			name: "Zelda Link To Past",
+			url: "files/videogame/songs/zelda_link-to-past.mp3"
+		},
+		{
+			id: 12,
+			category: "game",
+			name: "Dong KinKong Main",
+			url: "files/other/songs/dkng-main.mp3"
+		},
+		{
+			id: 13,
+			category: "game",
+			name: "Dong KinKong Other",
+			url: "files/other/songs/dkng-other.mp3"
+		},
+		{
+			id: 14,
+			category: "game",
+			name: "mega-man",
+			url: "files/other/songs/mega-man.mp3"
+		},
+		{
+			id: 15,
+			category: "cartoon",
+			name: "Flintstones",
+			url: "files/cartoons/songs/flintstones.mp3"
+		},
+		{
+			id: 16,
+			category: "cartoon",
+			name: "power-rangers",
+			url: "files/cartoons/songs/power-rangers.mp3"
+		},
+		{
+			id: 17,
+			category: "cartoon",
+			name: "simpsons",
+			url: "files/cartoons/songs/simpsons.mp3"
+		},
+		{
+			id: 18,
+			category: "cartoon",
+			name: "south-park",
+			url: "files/cartoons/songs/south-park.mp3"
+		},
+		{
+			id: 19,
+			category: "cartoon",
+			name: "thundercats",
+			url: "files/cartoons/songs/thundercats.mp3"
+		},
+		{
+			id: 20,
+			category: "cartoon",
+			name: "x-men",
+			url: "files/cartoons/songs/x-men.mp3"
 		}
 	];
 	const [src, setSrc] = useState("");
 	const [playing, setPlaying] = useState(false);
+	const [change, setChange] = useState(0);
 
 	const handleSongclick = index => {
 		let song = `https://assets.breatheco.de/apis/sound/${list[index].url}`;
 		setSrc(song);
-		setPlaying(true);
+		if (src !== "") {
+			player.current.play();
+			setPlaying(true);
+		} else {
+			setTimeout(() => {
+				player.current.play();
+				setPlaying(true);
+			}, 1000);
+		}
 		// console.log(player.current);
-		handlePlayClick();
+		// handlePlayClick();
+		setChange(index);
 	};
 
 	const handlePauseClick = () => {
@@ -50,7 +174,27 @@ export function Home() {
 			setTimeout(() => {
 				player.current.play();
 				setPlaying(true);
-			}, 100);
+			}, 1000);
+		}
+	};
+
+	const handleLeftClick = () => {
+		if (change > 0) {
+			let myNext = change - 1;
+			handleSongclick(myNext);
+			setChange(myNext);
+		} else {
+			alert("hit next button");
+		}
+	};
+
+	const handleRightClick = () => {
+		if (change <= list.length - 1) {
+			let myNext = change + 1;
+			handleSongclick(myNext);
+			setChange(myNext);
+		} else {
+			alert("hit previous button");
 		}
 	};
 
@@ -67,7 +211,11 @@ export function Home() {
 				})}
 			</ul>
 			<div className="nav" />
-			<i className="fas fa-caret-square-left mr-3" />
+			<i
+				className="fas fa-caret-square-left mr-3"
+				onClick={() => handleLeftClick()}
+			/>
+
 			{playing ? (
 				<i
 					className="fas fa-pause"
@@ -76,7 +224,10 @@ export function Home() {
 			) : (
 				<i className="fas fa-play" onClick={() => handlePlayClick()} />
 			)}
-			<i className="fas fa-caret-square-right ml-3" />
+			<i
+				className="fas fa-caret-square-right ml-3"
+				onClick={() => handleRightClick()}
+			/>
 		</div>
 	);
 }
